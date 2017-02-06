69887f4705f2c021492673c24a7a7b7d5f247d76
Vladimir Ignjatijevic <v.ignjatijevic@thalia.de>
2017-02-06 13:39:33 +0100

    amiga_cracktro_rsi: fixed code formatting

--- src/amiga_cracktro_rsi/main.js
+++ src/amiga_cracktro_rsi/main.js
@@ -1,19 +1,19 @@
 /**************************************************
-	Red Sector Inc. Amiga Cracktro Remake (v1)
-	------------------------------------------
-	Original code: Mr Zero Page
-	Original artwork & design: Red Sector Inc.
-	Original music: 'amegas' by Karsten Obarski
+    Red Sector Inc. Amiga Cracktro Remake (v1)
+    ------------------------------------------
+    Original code: Mr Zero Page
+    Original artwork & design: Red Sector Inc.
+    Original music: 'amegas' by Karsten Obarski
 
-	Javascript remake: Vladimir Ignjatijevic
-					   igvlada@gmail.com
-	CODEF framework: Antoine 'NoNameNo' Santo
-	                 http://codef.santo.fr
+    Javascript remake: Vladimir Ignjatijevic
+                       igvlada@gmail.com
+    CODEF framework: Antoine 'NoNameNo' Santo
+                     http://codef.santo.fr
 
-	Uses: codef_core.js
-		  codef_starfield.js
-		  codef_scrolltext.js
-		  codef_music.js
+    Uses: codef_core.js
+          codef_starfield.js
+          codef_scrolltext.js
+          codef_music.js
 ***************************************************/
 
 // basepath
@@ -31,13 +31,13 @@ var SCROLLER_RASTER_SPEED = 0.8;
 var SCROLLER_Y_TOP = 14;
 var SCROLLER_Y_BOTTOM = 186 - 14;
 var SCROLLER_TEXT = "THE GREAT GIANA SISTER WAS CRACKED BY MR ZERO PAGE@@@@@ "
-				  + "FOR THE COORPORATION TLC TCS AND RSI "
-				  + "I INVITE EVERYBODY TO CRACK THIS PROGRAM "
-				  + "BECAUSE IT WAS VERY FUNNY TO CRACK IT "
-				  + "GOOD IDEA TO PUT MORE SECTORS ON ONE TRACK "
-				  + "IF YOU NEED A COPY CONTACT ME  "
-				  + "THIS TIME A BIG HELLO TO THE MAN WHO IS CALLED TO WALK LIKE A LEGEND "
-				  + "YEAHHH PBA WHO HAS FORCES WITH RED SECTOR           ";
+                  + "FOR THE COORPORATION TLC TCS AND RSI "
+                  + "I INVITE EVERYBODY TO CRACK THIS PROGRAM "
+                  + "BECAUSE IT WAS VERY FUNNY TO CRACK IT "
+                  + "GOOD IDEA TO PUT MORE SECTORS ON ONE TRACK "
+                  + "IF YOU NEED A COPY CONTACT ME  "
+                  + "THIS TIME A BIG HELLO TO THE MAN WHO IS CALLED TO WALK LIKE A LEGEND "
+                  + "YEAHHH PBA WHO HAS FORCES WITH RED SECTOR           ";
 
 // canvases
 var screen;
@@ -56,10 +56,10 @@ var image_raster;
 // params
 var starfield_2d;
 var starfield_2d_params = [
-        { nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#cecece', speedy: 0, speedx: STARFIELD_SPEED },
-        { nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#737573', speedy: 0, speedx: STARFIELD_SPEED / 2 },
-        { nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#525552', speedy: 0, speedx: 0.0 },
-	];
+		{ nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#cecece', speedy: 0, speedx: STARFIELD_SPEED },
+		{ nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#737573', speedy: 0, speedx: STARFIELD_SPEED / 2 },
+		{ nb: STARFIELD_DENSITY, size: STARFIELD_DOT_SIZE, color:'#525552', speedy: 0, speedx: 0.0 },
+    ];
 var logo_direction;
 var logo_ypos1;
 var logo_ypos2;
@@ -82,7 +82,7 @@ function init() {
 	canvas_main = new canvas(320, 200);
 	canvas_scroller = new canvas(320, 14);
   
-  	// init gfx
+	// init gfx
 	image_logo_red = new image(basepath + 'rsc_logo_red.png');
 	image_logo_sector = new image(basepath + 'rsc_logo_sector.png');
 	image_raster = new image(basepath + 'rsc_font_raster.png');
@@ -106,9 +106,9 @@ function init() {
 	// init music player and start playing
 	player = new music("MK");
 	//player.stereo(true);
-    player.LoadAndRun(basepath + 'rsc_amegas.mod');
+	player.LoadAndRun(basepath + 'rsc_amegas.mod');
 
-    // start rendering
+	// start rendering
 	renderFrame();
 }
 
@@ -143,14 +143,14 @@ function renderFrame() {
 	logo_ypos1 += LOGO_SPEED * logo_direction;
 	logo_ypos2 -= LOGO_SPEED * logo_direction;	
 
-    if (logo_ypos1 > LOGO_BOUND_BOTTOM) {
-      	logo_direction = -1;
-    } else if (logo_ypos1 < LOGO_BOUND_TOP) {
-    	logo_direction = 1;
-    }
+	if (logo_ypos1 > LOGO_BOUND_BOTTOM) {
+		logo_direction = -1;
+	} else if (logo_ypos1 < LOGO_BOUND_TOP) {
+		logo_direction = 1;
+	}
 
-    // draw logos
-    if (logo_direction == 1) {
+	// draw logos
+	if (logo_direction == 1) {
 		image_logo_red.draw(canvas_main, 0, logo_ypos1, 1, 0, 1, 1);
 		image_logo_sector.draw(canvas_main, 0, logo_ypos2, 1, 0, 1, 1);
 	} else {
